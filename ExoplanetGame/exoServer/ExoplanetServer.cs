46ed0325fefe0c7f71b9dbd6832e67f43d7637d0
DenRay <drayker@gmx.de>
2024-02-20 14:39:28 +0100

    Removed confusing classes / Switch to basic TCP Connection

---
+++ ExoplanetGame/exoServer/ExoplanetServer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exoplanet.exoServer
+{
+    internal class ExoplanetServer
+    {
+        private static TcpListener server;
+        private static bool isRunning = true;
+
+        private static void Main(string[] args)
+        {
+            server = new TcpListener(IPAddress.Any, 9999);
+            server.Start();
+            Console.WriteLine("Exoplanet server started...");
+
+            while (isRunning)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Robot connected.");
+
+                Thread clientThread = new Thread(HandleClient!);
+                clientThread.Start(client);
+            }
+        }
+
+        private static void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int bytesRead;
+
+                try
+                {
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+                }
+                catch
+                {
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received from robot: " + dataReceived);
+
+                // Echo back to the robot
+                byte[] response = Encoding.ASCII.GetBytes("Received: " + dataReceived);
+                stream.Write(response, 0, response.Length);
+            }
+
+            client.Close();
+            Console.WriteLine("Robot disconnected.");
+        }
+    }
+}
--- RemoteRobot/RemoteRobot.cs
+++ RemoteRobot/RemoteRobot.cs
@@ -1,174 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
-using RemoteRobot.exo;
-using RemoteRobot.message;
-using RemoteRobot.tcp;
 
 namespace RemoteRobot
 {
     internal class RemoteRobot
     {
-        private ExoTcpClient controlServerClient;
-        private ExoTcpClient exoPlanetClient;
-        private PlanetSize planetSize;
-        private List<Position> allRobotPositions;
-        private Position robotPosition;
-        private Measure robotMeasure;
-
-        public RemoteRobot()
+        private static void Main(string[] args)
         {
-            StartClients();
-        }
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 9999);
 
-        private void StartClients()
-        {
-            allRobotPositions = new List<Position>();
+            NetworkStream stream = client.GetStream();
 
-            Console.WriteLine("Please enter IP-Address of Exo-Planet:");
-            string exoPlanetIP = Console.ReadLine();
+            Console.WriteLine("Connected to exoplanet server.");
 
-            if (string.IsNullOrEmpty(exoPlanetIP))
-                exoPlanetIP = "localhost";
-
-            StartExoPlanetClient(exoPlanetIP);
-        }
-
-        private void StartExoPlanetClient(string ip)
-        {
-            // Create an instance of ExoTcpClient
-            controlServerClient = new ExoTcpClient(ip, 7777);
-
-            // Subscribe to the MessageReceived event to handle incoming messages
-            controlServerClient.MessageReceived += processReceivedDataFromExoPlanet;
-        }
-
-        private void processReceivedDataFromExoPlanet(string receivedData)
-        {
-            Console.WriteLine("Received: " + receivedData);
-
-            ReceivedMessage receivedMessage = GenerateReceivedMessage(receivedData);
-
-            switch (receivedMessage.GetDataType())
+            while (true)
             {
-                case DataType.SIZE:
-                    planetSize = receivedMessage.GetMessageData().GetSize();
-                    break;
+                Console.Write("Enter message to send to exoplanet: ");
+                string? message = Console.ReadLine();
 
-                case DataType.POSITION:
-                    robotPosition = receivedMessage.GetMessageData().GetPosition();
-                    break;
+                if (message != null)
+                {
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                }
 
-                case DataType.MEASURE:
-                    robotMeasure = receivedMessage.GetMessageData().GetMeasure();
-                    break;
-
-                case DataType.DIRECTION:
-                    if (robotPosition == null)
-                        return;
-
-                    robotPosition.SetDir(receivedMessage.GetMessageData().GetDirection());
-                    break;
-
-                case DataType.CRASHED:
-                    DestroyRobot();
-                    break;
-
-                case DataType.UNKNOWN:
-                    break;
-            }
-        }
-
-        private ReceivedMessage GenerateReceivedMessage(string receivedData)
-        {
-            ReceivedMessage receivedMessage = new ReceivedMessage();
-
-            string[] splittedData = receivedData.Split(':');
-
-            switch (splittedData[0])
-            {
-                case "init":
-                    receivedMessage.SetDataType(DataType.SIZE);
-                    break;
-
-                case "landed":
-                case "scaned":
-                    receivedMessage.SetDataType(DataType.MEASURE);
-                    break;
-
-                case "moved":
-                case "pos":
-                    receivedMessage.SetDataType(DataType.POSITION);
-                    break;
-
-                case "rotated":
-                    receivedMessage.SetDataType(DataType.DIRECTION);
-                    break;
-
-                case "crashed":
-                    receivedMessage.SetDataType(DataType.CRASHED);
-                    break;
-
-                case "charged":
-                    receivedMessage.SetDataType(DataType.ROBOTSTATUS);
-                    break;
-
-                default:
-                    receivedMessage.SetDataType(DataType.UNKNOWN);
-                    break;
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Response from exoplanet: " + response);
             }
 
-            if (receivedMessage.GetDataType() == DataType.UNKNOWN)
-            {
-                return receivedMessage;
-            }
-
-            switch (receivedMessage.GetDataType())
-            {
-                case DataType.SIZE:
-                    receivedMessage.GetMessageData().SetSize(PlanetSize.Parse(splittedData[1]));
-                    break;
-
-                case DataType.MEASURE:
-                    receivedMessage.GetMessageData().SetMeasure(Measure.Parse(splittedData[1]));
-                    break;
-
-                case DataType.POSITION:
-                    receivedMessage.GetMessageData().SetPosition(Position.Parse(splittedData[1]));
-                    break;
-
-                case DataType.DIRECTION:
-                    Direction.TryParse(splittedData[1], out Direction newDirection);
-                    receivedMessage.GetMessageData().SetDirection(newDirection);
-                    break;
-
-                case DataType.ROBOTSTATUS:
-                    // TODO: receivedMessage.GetMessageData().SetRobotStatus();
-                    break;
-
-                case DataType.CRASHED:
-                case DataType.UNKNOWN:
-                    break;
-            }
-
-            return receivedMessage;
-        }
-
-        private void DestroyRobot()
-        {
-            Console.WriteLine("See you later alligator <3");
-            controlServerClient.Stop();
-            exoPlanetClient.Stop();
-            Environment.Exit(0);
-        }
-
-        public static void Main(string[] args)
-        {
-            new RemoteRobot();
+            client.Close();
         }
     }
 }
