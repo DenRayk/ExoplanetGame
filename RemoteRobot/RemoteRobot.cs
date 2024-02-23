49a499212ffbe8645efb5ef7d582cb18eb199bdc
BraeuerMax <maximilian@c-braeuer.de>
2024-02-23 14:04:15 +0100

    Added HandleResponse

--- RemoteRobot/RemoteRobot.cs
+++ RemoteRobot/RemoteRobot.cs
@@ -21,7 +21,34 @@ namespace RemoteRobot
 
         internal void HandleResponse(string response)
         {
-            //throw new NotImplementedException();
+            string[] parts = response.Split(':');
+            string commandName = parts[0];
+            string[] parameters = parts.Length > 1 ? parts[1].Split('|') : Array.Empty<string>();
+
+            switch (commandName)
+            {
+                case "init":
+                    Console.WriteLine($"Initializing with size: width = {parameters[1]}, height = {parameters[2]}");
+                    break;
+                case "landed":
+                    Console.WriteLine($"Landed: ground = {parameters[1]}");
+                    break;
+                case "scaned":
+                    Console.WriteLine($"Scanned: ground = {parameters[1]}");
+                    break;
+                case "moved":
+                    Console.WriteLine($"Moved: position = x:{parameters[1]}, y:{parameters[2]}, direction = {parameters[3]}");
+                    break;
+                case "rotated":
+                    Console.WriteLine($"Rotated: direction = {parameters[1]}");
+                    break;
+                case "crashed":
+                    Console.WriteLine("Crashed");
+                    break;
+                default:
+                    Console.WriteLine($"Unknown command: {commandName}");
+                    break;
+            }
         }
     }
 }
