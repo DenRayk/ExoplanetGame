428da75f63a67bf6860a68e2ddf0b31e175b96c9
DenRay <drayker@gmx.de>
2024-03-28 14:01:01 +0100

    Add class to save and laod topography

---
+++ ExoplanetGame/Exoplanet/TopogrpahyFileManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ExoplanetGame.Exoplanet
+{
+    public class TopographyFileManager
+    {
+        public void SaveTopographyToFile(string[] topography, string filePath)
+        {
+            try
+            {
+                File.WriteAllLines(filePath, topography);
+                Console.WriteLine("Topography saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving topography: {ex.Message}");
+            }
+        }
+
+        public string[] LoadTopographyFromFile(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string[] topography = File.ReadAllLines(filePath);
+                    Console.WriteLine("Topography loaded successfully.");
+                    return topography;
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading topography: {ex.Message}");
+            }
+
+            return null;
+        }
+    }
+}
