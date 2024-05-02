332aee4ef14cfabe38e321585b11f6d7288f68c0
DenRay <drayker@gmx.de>
2024-05-02 15:16:06 +0200

    Add weather descriptions

--- ExoplanetGame/Domain/Exoplanet/Environment/Weather.cs
+++ ExoplanetGame/Domain/Exoplanet/Environment/Weather.cs
@@ -1,12 +1,27 @@
-﻿namespace ExoplanetGame.Domain.Exoplanet.Environment;
+﻿using System.ComponentModel;
+
+namespace ExoplanetGame.Domain.Exoplanet.Environment;
 
 public enum Weather
 {
+    [Description("Sunny")]
     SUNNY,
+
+    [Description("Snowy")]
     SNOWY,
+
+    [Description("Rainy")]
     RAINY,
+
+    [Description("Windy")]
     WINDY,
+
+    [Description("Foggy")]
     FOGGY,
+
+    [Description("Cloudy")]
     CLOUDY,
+
+    [Description("Ash in the air")]
     ASH_IN_THE_AIR
 }
